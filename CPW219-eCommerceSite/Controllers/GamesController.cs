123632fdaed172256082b2c9308658ffae3db7b3
Scott <greatvstech@gmail.com>
2024-08-14 22:05:23 -0700

    Made a Create Game view

---
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
