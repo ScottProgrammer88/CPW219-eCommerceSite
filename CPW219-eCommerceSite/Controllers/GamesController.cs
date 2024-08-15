d085c82b717592def7f713d1461aa969e8cde5f7
Scott <greatvstech@gmail.com>
2024-08-15 12:32:31 -0700

    Added Edit view, HttpGet for edit, and hid the GameId from the User

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -21,7 +21,7 @@ namespace CPW219_eCommerceSite.Controllers
         }
 
         /// <summary>
-        /// This method displays the form to create a new game. It's a GET request, where the server sends the form to the client.
+        /// This method displays the form to create a new gameToEdit. It's a GET request, where the server sends the form to the client.
         /// </summary>
         /// <returns></returns>
         [HttpGet] 
@@ -31,21 +31,21 @@ namespace CPW219_eCommerceSite.Controllers
         }
 
         /// <summary>
-        /// This method handles the form submission when the user submits the data to create a new game.
+        /// This method handles the form submission when the user submits the data to create a new gameToEdit.
         /// It's a POST request, where the data is sent to the server. Handles form submission, validates data,
-        /// and either saves the game or returns the form with validation errors.
+        /// and either saves the gameToEdit or returns the form with validation errors.
         /// </summary>
         /// <param name="game"></param>
         /// <returns></returns>
         [HttpPost] 
-        public async Task<IActionResult> Create(Game game) // Model binding, this is a model object representing the game data submitted by the user. task is a type of object that represents an asynchronous operation.
+        public async Task<IActionResult> Create(Game game) // Model binding, this is a model object representing the gameToEdit data submitted by the user. task is a type of object that represents an asynchronous operation.
         {
             if (ModelState.IsValid)
             {
-                // Add the game to the database
-                _context.Games.Add(game); // Prepare the game to be saved to the database
+                // Add the gameToEdit to the database
+                _context.Games.Add(game); // Prepare the gameToEdit to be saved to the database
                 // For async code information, see https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
-                await _context.SaveChangesAsync(); // Save the game to the database. This is an async operation, so we use the await keyword. Add Async to the end of the method name.
+                await _context.SaveChangesAsync(); // Save the gameToEdit to the database. This is an async operation, so we use the await keyword. Add Async to the end of the method name.
 
                 // show the user a success message
                 ViewData["Message"] = $"{game.Title} was successfully added!";
@@ -53,5 +53,19 @@ namespace CPW219_eCommerceSite.Controllers
             }
             return View(game);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Game gameToEdit = await _context.Games.FindAsync(id); // Find the gameToEdit with the specified id
+
+            if (gameToEdit == null) // If the gameToEdit is not found in the database
+            {
+                return NotFound();  // or: RedirectToAction("Index"); Redirect the user to the Index action. 
+            }
+            
+            return View(gameToEdit);
+        }
+
+
     }
 }
